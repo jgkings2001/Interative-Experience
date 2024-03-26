7b3ae8c1d1a55d741284ca7d76b1152ca8073b55
jgkings2001 <joseph2.kingsbury@live.uwe.ac.uk>
2024-03-26 10:36:38 +0000

    add horror hit script

---
+++ Assets/Scripts/HorrorCrash.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorrorCrash : MonoBehaviour
+{
+    public AudioClip horrorHitSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("boo");
+        GetComponent<AudioSource>().PlayOneShot(horrorHitSound);
+
+
+    }
+}
