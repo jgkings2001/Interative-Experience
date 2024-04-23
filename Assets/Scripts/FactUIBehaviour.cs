355a7cbfc543680efc4300aa50fb817ff1c634dc
jgkings2001 <joseph2.kingsbury@live.uwe.ac.uk>
2024-04-23 11:49:31 +0100

    add assets and interaction

---
+++ Assets/Scripts/FactUIBehaviour.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactUIBehaviour : MonoBehaviour
+{
+    public GameObject ui;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+            ShowUI();
+    }
+
+    public void HideUI()
+    {
+        ui.SetActive(false);
+    }
+
+    public void ShowUI()
+    {
+        ui.SetActive(true);
+    }
+
+}
