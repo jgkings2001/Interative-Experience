b335319af22852649ac73835f0b2e1d3c6cfff1e
jgkings2001 <joseph2.kingsbury@live.uwe.ac.uk>
2024-05-03 20:03:54 +0100

    created pick up/drop script and added more assets

--- Assets/Scripts/PlayerPickUpDrop.cs
+++ Assets/Scripts/PlayerPickUpDrop.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class PlayerPickUpDrop : MonoBehaviour
 {
     [SerializeField] private Transform playerCameraTransform;
+    [SerializeField] private Transform objectGrabPointTransform;
     [SerializeField] private LayerMask pickUpLayerMask;
+
+    private ObjectGrabbable objectGrabbable;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
-        { float pickUpDistance = 2f;
-          if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickUpDistance = 2f, pickUpLayerMask));
-            Debug.Log(raycastHit.transform);
+        {
+            if (objectGrabbable == null)
+            {
+                float pickUpDistance = 2f;
+                if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickUpDistance = 2f, pickUpLayerMask)) ;
+                //Debug.Log(raycastHit.transform);
+                if (raycastHit.transform.TryGetComponent(out objectGrabbable)) {
+                    objectGrabbable.Grab(objectGrabPointTransform);
+                    //Debug.Log(objectGrabbable);
+                }
+            }
+        } else {
+            objectGrabbable.Drop();
+            objectGrabbable = null;
         }
     }
 }
